1657d687e4f7641045782bb5705378d86f2227f4
Dmytro Zinchuk <127327533+dmytro2104z@users.noreply.github.com>
2023-09-04 11:41:10 +0200

    HW_07.1.

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const placeElementRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+   </a>
+ </li>
+ `
+  )
+  .join("");
+
+placeElementRef.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
